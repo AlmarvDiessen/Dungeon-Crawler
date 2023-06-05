432acc6c5f348f01ae75ee5142f60f36f8f0550e
AlmarvDiessen <0330841@student.rocvantwente.nl>
2023-06-05 14:52:55 +0200

    DashComponent
    Dash component for dashable enemies
    some testing materials

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,11 +1,33 @@
 using Assets.Scripts;
+using Assets.Scripts.Interfaces;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Entity {
+public class Player : Entity, IDashable {
 
+    [SerializeField] private DashComponent dash;
+    [SerializeField] private BetterMovement playerMovement;
     private void Awake() {
 
+    }
+
+    private void Start() {
+        dash = GetComponent<DashComponent>();
+        playerMovement = GetComponent<BetterMovement>();
+
+    }
+    private void FixedUpdate() {
+        playerMovement.Movement();
+       
+    }
+
+    private void Update() {
+        PlayerDash();
+    }
+    public void PlayerDash() {
+        if(Input.GetKeyDown(KeyCode.LeftShift)&& dash.CanDash) {
+           dash.Dash();
+        }
     }
 }
