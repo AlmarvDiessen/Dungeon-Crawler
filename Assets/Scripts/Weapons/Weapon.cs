4b49b9a0a49ca249473a37732df4b43384250fdf
AlmarvDiessen <0330841@student.rocvantwente.nl>
2023-06-15 13:36:10 +0200

    weapons

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,8 +1,9 @@
+using Assets.Scripts.Interfaces;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Weapon : MonoBehaviour, IEquippable
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,9 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void EquipItem(IEquippable equipable) {
+
     }
 }
