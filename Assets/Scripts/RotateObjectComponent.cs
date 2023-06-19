533c217295a58cca15b345290a94e96ef70727a7
PauloBoe <Pboe41@gmail.com>
2023-06-19 14:32:39 +0200

    Last push for Key Pickup
    Going to Combat branch now

--- Assets/Scripts/RotateObjectComponent.cs
+++ Assets/Scripts/RotateObjectComponent.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class RotateObjectComponent : MonoBehaviour
 {
     [SerializeField] float rotateSpeed;
+    [SerializeField] float rotateUpSpeed;
 
     public float RotateSpeed { get => rotateSpeed; set => rotateSpeed = value; }
+    public float RotateUpSpeed { get => rotateUpSpeed; set => rotateUpSpeed = value; }
 
     void Update()
     {
         transform.Rotate(0, RotateSpeed * Time.deltaTime, 0);
+        transform.Rotate(0, 0, RotateUpSpeed * Time.deltaTime);
     }
 }
